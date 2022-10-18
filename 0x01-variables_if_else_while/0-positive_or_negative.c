a7042a63c8bc90af2aabd39e2fb1e48a9e998bcd
glory <glory.com>
2022-10-17 17:13:30 -0700

    yeah

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,9 +10,7 @@
 */ 
 int main(void)
 {
-	int n;
-
-	srand(time(0));
+	int n;	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
@@ -20,6 +18,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
-
 	return (0);
 }
